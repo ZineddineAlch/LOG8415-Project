1b7a1fb8c6d4b5f47cd2f23aa76c59d09e266b8b
Zineddine <zineddine.aliche.01@gmail.com>
2022-12-22 19:25:31 -0500

    small changes

--- launch.py
+++ launch.py
@@ -1,5 +1,5 @@
 import os
-
+import time
 os.system("IAM_sagemaker.py")
 os.system("IAM_lambda.py")
 os.system("file_zip.py")
@@ -8,4 +8,7 @@ os.system("create_S3_bucket.py")
 os.system("sagemaker_instance.py")
 os.system("API_gateway.py")
 os.system("Request.py")
+time.sleep(10)
 os.system("menu.py")
+time.sleep(30)
+
